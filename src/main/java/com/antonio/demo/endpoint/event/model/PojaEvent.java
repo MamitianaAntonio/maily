ca03b7e14e63a4c5aca6f7f8dc1196bf930acbfb
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-07-11 19:58:30 +0300

    fix : add missing number of attempt

--- src/main/java/com/antonio/demo/endpoint/event/model/PojaEvent.java
+++ src/main/java/com/antonio/demo/endpoint/event/model/PojaEvent.java
@@ -4,18 +4,14 @@ import static com.antonio.demo.endpoint.event.EventStack.EVENT_STACK_1;
 
 import com.antonio.demo.PojaGenerated;
 import com.antonio.demo.endpoint.event.EventStack;
-
+import java.io.Serializable;
+import java.time.Duration;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.io.Serializable;
-import java.time.Duration;
-
 @PojaGenerated
 public abstract class PojaEvent implements Serializable {
-  @Getter
-  @Setter
-  protected int attemptNb;
+  @Getter @Setter protected int attemptNb;
 
   public abstract Duration maxConsumerDuration();
 
@@ -42,8 +38,7 @@ public abstract class PojaEvent implements Serializable {
   }
 
   public String getEventSource() {
-    if (getEventStack().equals(EventStack.EVENT_STACK_1))
-      return "com.antonio.demo.event1";
+    if (getEventStack().equals(EventStack.EVENT_STACK_1)) return "com.antonio.demo.event1";
     return "com.antonio.demo.event2";
   }
 }
