50b365e970aeaf33240f48db86702f82e9a29f19
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-07-11 19:38:13 +0300

    refactor: replace sync HelloWorldController with async event-driven version

--- src/main/java/com/antonio/demo/endpoint/event/model/PojaEvent.java
+++ src/main/java/com/antonio/demo/endpoint/event/model/PojaEvent.java
@@ -1,9 +1,11 @@
 package com.antonio.demo.endpoint.event.model;
 
+import static com.antonio.demo.endpoint.event.EventStack.EVENT_STACK_1;
+
 import com.antonio.demo.PojaGenerated;
+import com.antonio.demo.endpoint.event.EventStack;
 import java.io.Serializable;
 import java.time.Duration;
-import com.antonio.demo.endpoint.event.EventStack;
 
 @PojaGenerated
 public abstract class PojaEvent implements Serializable {
@@ -32,7 +34,7 @@ public abstract class PojaEvent implements Serializable {
   }
 
   public String getEventSource() {
-    if (getEventStack().equals(EVENT_STACK_1)) return "com.antonio.demo.event1";
+    if (getEventStack().equals(EventStack.EVENT_STACK_1)) return "com.antonio.demo.event1";
     return "com.antonio.demo.event2";
   }
 }
